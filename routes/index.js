0efcfc222a6d9563dbba1494f075c200aab7b927
xiaoyujia-arch <xiaoyujia@cmail.carleton.ca>
2021-04-02 15:01:27 +0800

    make it better

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.post('/addnew', actions.addNewUser)
 router.post('/auth', actions.authentication)
 
 //get health status on a user
-router.get('/getstatus', actions.getstatus)
+router.post('/getstatus', actions.getstatus)
 
 //user location update
 router.post('/locationupdate', actions.locationUpdate)
